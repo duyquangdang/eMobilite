b00c24427d8984ad4615c6804d5fbc1b238c6304
Romuald LEMESLE <romuald.lemesle@byzaneo.com>
2016-04-03 21:27:12 +0200

    delete NFC

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'angular-svg-round-progress', 'ngMessages', 'nfcFilters'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'angular-svg-round-progress', 'ngMessages'])
 
 .run(function($ionicPlatform, $ionicPopup) {
 	/*$ionicPlatform.registerBackButtonAction(function(event) {
@@ -9,7 +9,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       })
     }
   }, 100);*/
-  
+
   $ionicPlatform.ready(function() {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 	  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -41,21 +41,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     templateUrl: 'templates/password.html',
     controller: 'PasswordCtrl'
   })
-  
+
   // Tables
   $stateProvider.state('tables', {
     url: '/tables:listTables',
     templateUrl: 'templates/tables.html',
     controller: 'TablesCtrl'
   })
-  
+
  // Pizzas
   $stateProvider.state('pizzas', {
     url: '/pizzas/:pizzasMkd:ingredients:tableID:tableNum:commandes',
     templateUrl: 'templates/pizzas.html',
     controller: 'PizzasCtrl'
   })
-  
+
   // PizzaCustom
   $stateProvider.state('pizzaCustom', {
     url: '/:pizzasMkd:ingredients:tableID:tableNum:pizzaId/pizzaCustom',
@@ -76,28 +76,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     templateUrl: 'templates/desserts.html',
     controller: 'DessertsCtrl'
   })
-  
+
   // Cuisine
   $stateProvider.state('cuisine', {
     url: '/cuisine',
     templateUrl: 'templates/cuisine.html',
     controller: 'CuisineCtrl'
   })
-  
+
    // Serveur
   $stateProvider.state('serveur', {
     url: '/serveur',
     templateUrl: 'templates/serveur.html',
     controller: 'ServeurCtrl'
   })
-  
+
   // Timer
   $stateProvider.state('timer', {
     url: '/timer',
     templateUrl: 'templates/timer.html',
     controller: 'TimerCtrl'
   })
-  
+
   // Template par defaut
   $urlRouterProvider.otherwise('/home')
 
---
+++ www/js/helper.js
@@ -0,0 +1,33 @@
+/**
+ * Created by romuald on 03/04/2016.
+ */
+/* Pizzas */
+function showAll(pizzasMkd) {
+  var principal = document.getElementById("principal");
+  var divPizzaPrincipal = document.createElement("div");
+  divPizzaPrincipal.setAttribute("id", "divPizzas");
+  var divPizzas = document.createElement("div");
+  for(var i in pizzasMkd) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "pizza col-25");
+    var img = document.createElement("img");
+    img.setAttribute("src", "img/pizzas/"+pizzasMkd[i].img);
+    img.setAttribute("onclick", "alert('test')");
+    var h4 = document.createElement("h4");
+    h4.innerHTML = pizzasMkd[i].name
+    div.appendChild(img);
+    div.appendChild(h4);
+    divPizzas.appendChild(div);
+    /* Add Row */
+    if((parseInt(i)+1)%4 == 0) {
+      divPizzas.setAttribute("class", "row");
+      divPizzaPrincipal.appendChild(divPizzas);
+      divPizzas = document.createElement("div");
+    }
+  }
+  divPizzas.setAttribute("class", "row");
+  divPizzaPrincipal.appendChild(divPizzas);
+  principal.insertBefore(divPizzaPrincipal, document.getElementById('button'));
+}
+
+/* Add category to pizza */
